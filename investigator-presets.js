404a1f7a6f2bff6ad752f6c5b234871cb656cd48
zxcvea <teetnclean@gmail.com>
2021-08-13 13:28:18 +1000

    no message

---
+++ investigator-presets.js
@@ -0,0 +1,110 @@
+var investigators = [
+  {
+    "Name": "Jack Exley",
+    "Job": "Detective",
+    "Image": "img/profile-detective.webp",
+    "Ability": "Once per round, you may use or give an additional action to another investigator.",
+    "Stats": {
+      "Damage": 7,
+      "Horror": 7
+    },
+    "Attributes": {
+      "Strength": 4,
+      "Agility": 3,
+      "Observation": 5,
+      "Lore": 3,
+      "Influence": 5,
+      "Will": 4
+    }
+  },
+  {
+    "Name": "Florence Nightingale",
+    "Job": "Nurse",
+    "Image": "img/profile-nurse.webp",
+    "Ability": "Once per round, you may heal you or another investigator. Discard Horror and/or Damage equal to a {will} check.",
+    "Stats": {
+      "Damage": 6,
+      "Horror": 8
+    },
+    "Attributes": {
+      "Strength": 3,
+      "Agility": 4,
+      "Observation": 5,
+      "Lore": 5,
+      "Influence": 5,
+      "Will": 5
+    }
+  },
+  {
+    "Name": "Frank Selak",
+    "Job": "Gambler/Highroller",
+    "Image": "img/profile-gambler.webp",
+    "Ability": "When resolving a test, you may convert any {?} to {success} and reroll any failed die at the cost of receiving 1 Damage or Horror.",
+    "Stats": {
+      "Damage": 5,
+      "Horror": 5
+    },
+    "Attributes": {
+      "Strength": 5,
+      "Agility": 5,
+      "Observation": 5,
+      "Lore": 5,
+      "Influence": 5,
+      "Will": 5
+    }
+  },
+  {
+    "Name": "Edith Crawford",
+    "Job": "Governess",
+    "Image": "img/profile-governess.webp",
+    "Ability": "Once per round, you or another investigator may reroll any failed die when resolving a test.",
+    "Stats": {
+      "Damage": 5,
+      "Horror": 9
+    },
+    "Attributes": {
+      "Strength": 2,
+      "Agility": 2,
+      "Observation": 5,
+      "Lore": 4,
+      "Influence": 4,
+      "Will": 4
+    }
+  },
+  {
+    "Name": "Private Foley Jackson",
+    "Job": "Soldier",
+    "Image": "img/profile-soldier.webp",
+    "Ability": "When attacking a monster, you deal 1 damage per {success} in addition to the damage result. If you fail a test, you deal 1 damage.",
+    "Stats": {
+      "Damage": 7,
+      "Horror": 7
+    },
+    "Attributes": {
+      "Strength": 5,
+      "Agility": 5,
+      "Observation": 2,
+      "Lore": 2,
+      "Influence": 3,
+      "Will": 4
+    }
+  },
+  {
+    "Name": "Edith Crawford",
+    "Job": "Damsel in distress",
+    "Image": "img/profile-damsel.webp",
+    "Ability": "{action} You may call for backup. Move another investigator to your space and they become {focused} and may discard 1 Damage or Horror.",
+    "Stats": {
+      "Damage": 5,
+      "Horror": 9
+    },
+    "Attributes": {
+      "Strength": 2,
+      "Agility": 2,
+      "Observation": 5,
+      "Lore": 4,
+      "Influence": 4,
+      "Will": 4
+    }
+  }
+];
---
+++ mom-investigators.js
@@ -0,0 +1,146 @@
+$(document).ready(function() {
+  InvestigatorSheet.Init();
+});
+
+var InvestigatorSheet = {
+
+  DEFAULT_WIDTH: 1406,
+  DEFAULT_HEIGHT: 815,
+  WINDOW_WIDTH: 0,
+  WINDOW_HEIGHT: 0,
+  INVESTIGATOR_INDEX: 0,
+
+  Setup: function() {
+    var container = '<div id="main"><div id="container"><div id="inner"></div></div></div>';
+    $('body').append(container);
+    $('#inner').width(investigators.length * $('#container').width());
+  },
+
+  BuildTemplate: function(investigator) {
+    var profile = '<div id="profile-image"><img src="' + investigator.Image + '" /></div><div id="profile-name" class="noselect">' + investigator.Name + '</div><div id="profile-job" class="noselect">' + investigator.Job + '</div>';
+    var ability = '<div id="ability" class="noselect">' + Dictionary.Replace(investigator.Ability) + '</div>';
+    var stats = '<div id="stats"><div id="stats-damage" class="stat-' + investigator.Stats.Damage + '"></div><div id="stats-horror" class="stat-' + investigator.Stats.Horror + '"></div></div>';
+    var attributes = '<div id="attributes"><div id="attributes-strength" class="attribute-' + investigator.Attributes.Strength + '"></div><div id="attributes-agility" class="attribute-' + investigator.Attributes.Agility + '"></div><div id="attributes-observation" class="attribute-' + investigator.Attributes.Observation + '"></div><div id="attributes-lore" class="attribute-' + investigator.Attributes.Lore + '"></div><div id="attributes-influence" class="attribute-' + investigator.Attributes.Influence + '"></div><div id="attributes-will" class="attribute-' + investigator.Attributes.Will + '"></div></div>';
+
+    var template = '<div class="template">' + profile + ability + stats + attributes + '</div>';
+    $('#inner').append(template);
+  },
+
+  CreateInvestigators: function() {
+    jQuery.each(investigators, function(index, investigator) {
+      InvestigatorSheet.BuildTemplate(investigator);
+    });
+  },
+
+  Swipe: function(direction) {
+    if (direction == 'right' && InvestigatorSheet.INVESTIGATOR_INDEX == (investigators.length - 1)) {
+      return;
+    }
+
+    if (direction == 'left' && InvestigatorSheet.INVESTIGATOR_INDEX == 0) {
+      return;
+    }
+
+    InvestigatorSheet.INVESTIGATOR_INDEX = (direction == 'right') ? InvestigatorSheet.INVESTIGATOR_INDEX + 1 : InvestigatorSheet.INVESTIGATOR_INDEX - 1;
+    var leftPos = parseInt($('#inner').css('left'));
+    if (direction == 'right') {
+      movePos = leftPos - $('#container').width();
+    } else {
+      movePos = leftPos + $('#container').width();
+    }
+    $('#inner').animate({
+      left: movePos + 'px'
+    }, 300);
+  },
+
+  Scale: function() {
+    var scale, origin;
+
+    var ratio = $(window).width() / $(window).height();
+    if (ratio >= 1 && ($(window).width() > InvestigatorSheet.DEFAULT_WIDTH)) {
+      scale = 1;
+    } else if (ratio >= 1) {
+      scale = $(window).width() / InvestigatorSheet.DEFAULT_WIDTH;
+    } else if (ratio < 1 && ($(window).height() > InvestigatorSheet.DEFAULT_WIDTH)) {
+      scale = 1;
+    } else if (ratio < 1 ) {
+      scale = $(window).height() / InvestigatorSheet.DEFAULT_WIDTH;
+    }
+
+    $('#container').css({
+      transform: "translate(-50%, -50%) " + "scale(" + scale + ")"
+    });
+
+    $('#inner').width(investigators.length * $('#container').width());
+  },
+
+  Events: function() {
+    $(document).click(function() {
+      if ($('#container').width() != InvestigatorSheet.WINDOW_WIDTH || $('#container').height() != InvestigatorSheet.WINDOW_HEIGHT) {
+        InvestigatorSheet.Scale();
+      }
+    });
+
+    var xStart, yStart, xEnd, yEnd;
+    $(document).on('mousedown touchstart', function(e) {
+      //console.log("(x,y) = (" + e.pageX + "," + e.pageY +")");
+      xStart = e.pageX;
+      yStart = e.pageY;
+    }).on('mouseup touchend', function(e) {
+      //console.log("(x,y) = (" + e.pageX + "," + e.pageY +")");
+      var ratio = $(window).width() / $(window).height();
+      xEnd = e.pageX;
+      yEnd = e.pageY;
+      if ((xEnd - xStart) > 50 && ratio >= 1) {
+        console.log('left');
+        InvestigatorSheet.Swipe('left');
+      } else if ((xEnd - xStart) < -50 && ratio >= 1) {
+        console.log('right');
+        InvestigatorSheet.Swipe('right');
+      } else if ((yEnd - yStart) > 50 && ratio < 1) {
+        console.log('up');
+        InvestigatorSheet.Swipe('left');
+      } else if ((yEnd - yStart) < -50 && ratio < 1) {
+        console.log('down');
+        InvestigatorSheet.Swipe('right');
+      }
+    })
+   ;
+  },
+
+  Init: function() {
+    InvestigatorSheet.Setup();
+    InvestigatorSheet.CreateInvestigators();
+    InvestigatorSheet.Events();
+    //InvestigatorSheet.Scale();
+  }
+
+};
+
+var Dictionary = {
+
+  Replace: function (text) {
+    text = text.replace('{action}', '<strong>Action:</strong>');
+    text = text.replace('{focused}', '<strong>focused</strong>');
+    text = text.replace('{dazed}', '<strong>dazed</strong>');
+    text = text.replace('{fearless}', '<strong>fearless</strong>');
+    text = text.replace('{mesmerized}', '<strong>mesmerized</strong>');
+    text = text.replace('{poisoned}', '<strong>poisoned</strong>');
+    text = text.replace('{restrained}', '<strong>restrained</strong>');
+    text = text.replace('{righteous}', '<strong>righteous</strong>');
+    text = text.replace('{stressed}', '<strong>stressed</strong>');
+    text = text.replace('{stunned}', '<strong>stunned</strong>');
+    text = text.replace('{wounded}', '<strong>wounded</strong>');
+    text = text.replace('{insane}', '<strong>insane</strong>');
+    text = text.replace('{strength}', '<span class="strength"></span>');
+    text = text.replace('{agility}', '<span class="agility"></span>');
+    text = text.replace('{observation}', '<span class="observation"></span>');
+    text = text.replace('{lore}', '<span class="lore"></span>');
+    text = text.replace('{influence}', '<span class="influence"></span>');
+    text = text.replace('{will}', '<span class="will"></span>');
+    text = text.replace('{?}', '<span class="investigation"></span>');
+    text = text.replace('{success}', '<span class="success"></span>');
+    return text;
+  }
+
+};
